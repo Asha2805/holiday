02d3785ec82931aab71f89fd8508c33e6da7ca3b
Asha2805 <43291441+Asha2805@users.noreply.github.com>
2018-09-27 12:08:35 +0530

    Update and rename README.md to holiday.java

---
+++ holiday.java
@@ -0,0 +1,18 @@
+import java.util.*;
+public class holiday
+{
+public static void main(String[] args)
+{
+String input;
+Scanner q=new Scanner(System.in);
+input=q.nextLine();
+if(String=="Saturday"&&"Sunday")
+{
+System.out.println("Yes");
+}
+else
+{
+System.out.println("No");
+}
+}
+}
